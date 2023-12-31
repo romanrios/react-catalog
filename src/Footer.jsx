84f1a7394d39bfbacfdf85ab898d1afdb9e696f0
Roman Rios <romanrios@live.com>
2023-12-31 16:37:23 -0300

    del all

--- src/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return <footer>
-        <p><i>Desarrollo de aplicaciones con React y React Native</i></p>
-        <p> MIRÍADAX_ | Universidad Politécnica de Madrid</p>
-        <a href="https://romanrios.github.io/">&copy; 2023 Román Ríos</a>
-    </footer>
-}
